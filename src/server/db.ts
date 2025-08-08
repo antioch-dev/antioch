fba05a9151523297bf50a75ab6cc2a120be0ced0
paulk_naanac <paulkachule@hotmail.com>
2025-08-08 15:59:16 +0800

    let prisma use var

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,16 +1,17 @@
-import { PrismaClient } from '@prisma/client';
-import { env } from '@/env';
+/* eslint-disable no-var */
+import { PrismaClient } from '@prisma/client'
+import { env } from '@/env'
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined
 }
 
 const prismaClient = new PrismaClient({
   log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-});
+})
 
-export const db = global.prisma ?? prismaClient;
+export const db = global.prisma ?? prismaClient
 
 if (env.NODE_ENV !== 'production') {
-  global.prisma = db;
+  global.prisma = db
 }
