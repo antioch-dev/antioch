0bc519ef61ad451a40c9fdf3e181f9023df08a57
Joshypeace <joshuachilapondwa2@gmail.com>
2025-09-24 16:47:31 +0200

    changed bible study layout from Root Layout structure to a a normal page Layout structure

--- src/app/[fellowship_id]/bible-study/layout.tsx
+++ src/app/[fellowship_id]/bible-study/layout.tsx
@@ -1,20 +1,11 @@
-import type { Metadata } from 'next'
-import '@/styles/globals.css'
-
-export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
-
-export default function RootLayout({
+export default function Layout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <section className="min-h-screen bg-background">
+      {children}
+    </section>
   )
 }
