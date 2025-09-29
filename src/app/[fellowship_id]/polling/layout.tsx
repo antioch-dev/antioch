b397500952641be2d5dfee629bbee6565846bafb
Joshypeace <joshuachilapondwa2@gmail.com>
2025-09-29 08:18:25 +0200

    changed polling RoolLayout to normal Layout

--- src/app/[fellowship_id]/polling/layout.tsx
+++ src/app/[fellowship_id]/polling/layout.tsx
@@ -1,30 +1,15 @@
 import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
 import { SidebarProvider } from "@/components/ui/sidebar"
 import { ThemeProvider } from "@/components/theme-provider"
 
-const inter = Inter({ subsets: ["latin"] })
-
-export const metadata: Metadata = {
-  title: "QuickPoll Analytics",
-  description: "Create and analyze questionnaires with ease",
-    generator: 'v0.app'
-}
-
-export default function RootLayout({
+export default function Layout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <SidebarProvider>{children}</SidebarProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <SidebarProvider>{children}</SidebarProvider>
+    </ThemeProvider>
   )
 }
